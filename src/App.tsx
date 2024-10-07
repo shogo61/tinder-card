77ce81b2152ecf67bf6bcfc934aeae9e11c191bc
shogo61 <shogo.611.lol@gmail.com>
2024-10-07 23:58:37 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import Advanced from "./components/Advanced";
+const App = () => (
+  <div className="app">
+    <Advanced />
+    <div className="row">
+      <p style={{ color: "#fff" }}>Show advanced example</p>{" "}
+    </div>
+  </div>
+);
+export default App;
